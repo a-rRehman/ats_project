3f715bd62e2ecd85eef7bfaee9a34e0681ae1d21
a-rRehman <arj00789@gmail.com>
2023-10-10 11:03:44 +0500

    Vuex and api added

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const BASE_URL = "http://10.0.10.211:3300/api";
+
+const ApiServices = {};
+
+export default ApiServices;
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+import book_module from "./modules/book_module";
+import boxes from "./modules/boxes_module";
+import HotSells from "./modules/HotSells";
+import user_module from "./modules/user_module";
+import cart_module from "./modules/cart_module";
+import wishlist_module from "./modules/wishlist_module";
+import wishList from "./modules/wishList";
+
+export default createStore({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
